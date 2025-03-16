cdc443dc5f7f50901bdfc6ac22bd9b951459e26b
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-03-16 11:49:57 -0600

    creacion de actividades diarias

--- Proyecto/src/proyecto/ActividadesDiarias.java
+++ Proyecto/src/proyecto/ActividadesDiarias.java
@@ -1,48 +1,36 @@
 
 package proyecto;
 
+import java.util.Random;
 
 public class ActividadesDiarias {
-    
-    String nombre;
-    int dia; 
 
-    public ActividadesDiarias(String nombre, int dia) {
-        this.nombre = nombre;
-        this.dia = dia;
-    }
-
-    String lista[]= new String[8];
+    public void dia() {
+             int dia;
+        String lista[]= new String[6];
     //llenado de la lista de actividades: 
-   
+    //estaba haciendo eso pero no se que le paso a mi compu y no me deja usar las variables
+    //por lo que voy a escribir el código dentro de un metodo para que si alguno les sirve sin el metodo lo saque
     
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getDia() {
-        return dia;
-    }
-
-    public void setDia(int dia) {
-        this.dia = dia;
-    }
-
-    public String[] getLista() {
-        return lista;
-    }
-
-    public void setLista(String[] lista) {
-        this.lista = lista;
-    }
-
- 
+        lista[0]="Se realizaran actividades en la piscina";
+        lista[1]= "Se enseñaran trucos al perrito";
+        lista[2]= "Se hará una carrera de obstaculos entre los perritos";
+        lista[3]= "Se realizara la limpieza semanal de los perros";
+        lista[4]="Se realizara un masaje a su perro";
+        lista[5]="Los perros jugaran al aire libre";
+        Random aleatorio = new Random();
+        int aux = 0;
+        aux = aleatorio.nextInt(0, 6);
+        dia=aux;
     
-    
+}
+
+    @Override
+    public String toString(String lista[], int dia) {
+        return super.toString()
+                    +"-------Actividad diaria------"
+                            +lista[dia];
+    }
+        
     
 }
