4959bf1e077305b6a76b1d38c4ac0dd87104ead7
Pablo Arias <parias40359@ufide.ac.cr>
2025-03-21 18:28:08 -0600

    Se agregó el Implements "ServicioAdicional" para que así además de la Herencia implemente los métodos

--- Proyecto/src/proyecto/Cliente.java
+++ Proyecto/src/proyecto/Cliente.java
@@ -1,62 +1,44 @@
 package proyecto;
 
+class Cliente extends Perro implements ServicioAdicional {
 
-public class Cliente extends Perro {
-    
-    private String nombre;
+    private String nombreDueño;
     private String telefono;
     private String correo;
+    private String habitacion;
 
-    public Cliente(String nombre, String telefono, String correo, 
-            //CLASE PERRO
-            String nom, int edad, String raza, String tamano, 
-            //CLASE RESERVA
+    public Cliente(String nombreDueño, String telefono, String correo,
+            String nombrePerro, int edad, String raza, String tamano,
             String fechaIngreso, String fechaSalida, int dias, int numReserva, double Descuentos, double costoTotal) {
-        
-        super(nom, edad, raza, tamano, fechaIngreso, fechaSalida, dias, numReserva, Descuentos, costoTotal);
-        this.nombre = nombre;
+        super(nombrePerro, edad, raza, tamano, fechaIngreso, fechaSalida, dias, numReserva, Descuentos, costoTotal);
+        this.nombreDueño = nombreDueño;
         this.telefono = telefono;
         this.correo = correo;
     }
 
-    
-   
-   
-       
-   
-
-    public String getCorreo() {
-        return correo;
+    @Override
+    public void asignarHabitacion() {
+        this.habitacion = "Habitación Premium"; // Ejemplo de asignación
     }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    @Override
+    public void aplicarDescuento() {
+        System.out.println("Aplicando descuento para " + nombreDueño);
     }
 
     @Override
     public String toString() {
-        return super.toString() +
-                "\n---------Dueño del perro: ------------"
-                +"\nNombre del dueño: "+ getNombre() 
-                + "\nNumero de telefono" + getTelefono() 
-                + "\nCorreo Electrónico" + getCorreo();
+        String habitacionTexto;
+        if (habitacion != null) {
+            habitacionTexto = habitacion;
+        } else {
+            habitacionTexto = "No asignada";
+        }
+
+        return super.toString() + "\n---------Dueño del perro: ------------"
+                + "\nNombre: " + nombreDueño
+                + "\nTeléfono: " + telefono
+                + "\nCorreo Electrónico: " + correo
+                + "\nHabitación asignada: " + habitacionTexto;
     }
-    
-    
 }
