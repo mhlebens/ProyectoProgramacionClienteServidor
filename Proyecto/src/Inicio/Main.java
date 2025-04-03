cdf27285f344752175d5d82fac25a1f25e97400c
mhlebens <marifermatah@gmail.com>
2025-04-03 16:40:12 -0600

    Ordené por paquetes las clases, para más orden.

---
+++ Proyecto/src/Inicio/Main.java
@@ -0,0 +1,16 @@
+
+package Inicio;
+
+//Esto es para llamar los diferentes paquetes
+import Entidades.*;
+import Hilo.*;
+import Servicios.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+      
+    }
+    
+}
