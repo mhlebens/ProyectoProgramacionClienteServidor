4171c1a6dfaf3fc1173648a1093d54ab40512b46
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-04-05 14:07:00 -0600

    creacion de getters y setters

--- Proyecto/src/proyecto/Cliente.java
+++ Proyecto/src/proyecto/Cliente.java
@@ -2,19 +2,47 @@ package proyecto;
 
 class Cliente extends Perro implements ServicioAdicional {
 
+    //Atributos de la clase Cliente
     private String nombreDueño;
     private String telefono;
     private String correo;
     private String habitacion;
-
+        
+    //Constructor de la clas Cliente
     public Cliente(String nombreDueño, String telefono, String correo,
+            //constructor de la clase perro
             String nombrePerro, int edad, String raza, String tamano,
+            //Constructor de la clase RESERVA 
             String fechaIngreso, String fechaSalida, int dias, int numReserva, double Descuentos, double costoTotal) {
         super(nombrePerro, edad, raza, tamano, fechaIngreso, fechaSalida, dias, numReserva, Descuentos, costoTotal);
         this.nombreDueño = nombreDueño;
         this.telefono = telefono;
         this.correo = correo;
     }
+    
+     public String getCorreo() {
+         return correo;
+     }
+ 
+     public void setCorreo(String correo) {
+         this.correo = correo;
+     }
+ 
+     public String getNombreDueño() {
+         return nombreDueño;
+     }
+ 
+     public void setNombreDueño(String nombreDueño) {
+         this.nombreDueño = nombreDueño;
+     }
+ 
+     public String getTelefono() {
+         return telefono;
+     }
+ 
+     public void setTelefono(String telefono) {
+         this.telefono = telefono;
+     }
 
     @Override
     public void asignarHabitacion() {
@@ -23,7 +51,7 @@ class Cliente extends Perro implements ServicioAdicional {
 
     @Override
     public void aplicarDescuento() {
-        System.out.println("Aplicando descuento para " + nombreDueño);
+        System.out.println("Aplicando descuento para " + getNombreDueño());
     }
 
     @Override
@@ -36,9 +64,9 @@ class Cliente extends Perro implements ServicioAdicional {
         }
 
         return super.toString() + "\n---------Dueño del perro: ------------"
-                + "\nNombre: " + nombreDueño
-                + "\nTeléfono: " + telefono
-                + "\nCorreo Electrónico: " + correo
+                + "\nNombre: " + getNombreDueño()
+                + "\nTeléfono: " + getTelefono()
+                + "\nCorreo Electrónico: " + getCorreo()
                 + "\nHabitación asignada: " + habitacionTexto;
     }
 }
