7078cbce777c70f85d4ede509d459fbf4bad4df9
Pablo Arias <parias40359@ufide.ac.cr>
2025-04-13 15:24:15 -0600

    Se agrego un metodo y mas actividades

--- Proyecto/src/Servicios/ActividadesDiarias.java
+++ Proyecto/src/Servicios/ActividadesDiarias.java
@@ -10,13 +10,38 @@ public class ActividadesDiarias {
         "Se hará una carrera de obstáculos entre los perritos",
         "Se realizará la limpieza semanal de los perros",
         "Se realizará un masaje a su perro",
-        "Los perros jugarán al aire libre"
+        "Los perros jugarán al aire libre",
+        "Se tomarán fotos profesionales de los perritos",
+        "Se realizará una caminata guiada con los cuidadores",
+        "Hora de juegos con pelotas y juguetes",
+        "Se preparará comida especial para los perritos",
+        "Momento de relajación con música suave",
+        "Pintura de huellitas para recuerdo del dueño"
     };
 
     public String obtenerActividadDelDia() {
         Random aleatorio = new Random();
         int dia = aleatorio.nextInt(ACTIVIDADES.length);
         return ACTIVIDADES[dia];
+    }
+    
+    public String[] generarListaSinRepetir(int cantidad) {
+        String[] copia = new String[ACTIVIDADES.length];
+        boolean[] usados = new boolean[ACTIVIDADES.length];
+        Random rand = new Random();
+        String[] resultado = new String[Math.min(cantidad, ACTIVIDADES.length)];
+
+        int count = 0;
+        while (count < resultado.length) {
+            int index = rand.nextInt(ACTIVIDADES.length);
+            if (!usados[index]) {
+                resultado[count] = ACTIVIDADES[index];
+                usados[index] = true;
+                count++;
+            }
+        }
+
+        return resultado;
     }
 
     @Override
