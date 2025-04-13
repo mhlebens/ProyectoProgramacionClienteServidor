170f8c511cdcce45a1f40220137e4a14048e67d8
Pablo Arias <parias40359@ufide.ac.cr>
2025-04-13 15:30:52 -0600

    Se cambiaron los nombres de campos y botones

--- Proyecto/src/InterfazGrafica/Actividades.java
+++ Proyecto/src/InterfazGrafica/Actividades.java
@@ -12,12 +12,12 @@ public class Actividades extends javax.swing.JFrame {
         ActividadesDiarias actividades = new ActividadesDiarias();
 
         // Llenar los 6 campos de texto con una actividad diferente
-        jTextField1.setText(actividades.obtenerActividadDelDia());
-        jTextField2.setText(actividades.obtenerActividadDelDia());
-        jTextField3.setText(actividades.obtenerActividadDelDia());
-        jTextField4.setText(actividades.obtenerActividadDelDia());
-        jTextField5.setText(actividades.obtenerActividadDelDia());
-        jTextField6.setText(actividades.obtenerActividadDelDia());
+        txtActividad.setText(actividades.obtenerActividadDelDia());
+        txtActividad2.setText(actividades.obtenerActividadDelDia());
+        txtActividad3.setText(actividades.obtenerActividadDelDia());
+        txtActividad4.setText(actividades.obtenerActividadDelDia());
+        txtActividad5.setText(actividades.obtenerActividadDelDia());
+        txtActividad6.setText(actividades.obtenerActividadDelDia());
     }
 
     @SuppressWarnings("unchecked")
@@ -28,20 +28,20 @@ public class Actividades extends javax.swing.JFrame {
         jPanel2 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        txtActividad2 = new javax.swing.JTextField();
         jLabel10 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtActividad = new javax.swing.JTextField();
         jLabel11 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
+        txtActividad3 = new javax.swing.JTextField();
         jLabel12 = new javax.swing.JLabel();
-        jTextField4 = new javax.swing.JTextField();
+        txtActividad4 = new javax.swing.JTextField();
         jLabel13 = new javax.swing.JLabel();
-        jTextField5 = new javax.swing.JTextField();
+        txtActividad5 = new javax.swing.JTextField();
         jLabel14 = new javax.swing.JLabel();
-        jTextField6 = new javax.swing.JTextField();
+        txtActividad6 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        Listar = new javax.swing.JButton();
+        NuevaLista = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         mbPrincipal = new javax.swing.JMenu();
         mitRegresarMP = new javax.swing.JMenuItem();
@@ -65,38 +65,38 @@ public class Actividades extends javax.swing.JFrame {
         jLabel9.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
         jLabel9.setText("Actividad N°1");
 
-        jTextField2.setBackground(new java.awt.Color(220, 201, 201));
-        jTextField2.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
+        txtActividad2.setBackground(new java.awt.Color(220, 201, 201));
+        txtActividad2.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
 
         jLabel10.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
         jLabel10.setText("Actividad N°2");
 
-        jTextField1.setBackground(new java.awt.Color(220, 201, 201));
-        jTextField1.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
+        txtActividad.setBackground(new java.awt.Color(220, 201, 201));
+        txtActividad.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
 
         jLabel11.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
         jLabel11.setText("Actividad N°3");
 
-        jTextField3.setBackground(new java.awt.Color(220, 201, 201));
-        jTextField3.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
+        txtActividad3.setBackground(new java.awt.Color(220, 201, 201));
+        txtActividad3.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
 
         jLabel12.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
         jLabel12.setText("Actividad N°4");
 
-        jTextField4.setBackground(new java.awt.Color(220, 201, 201));
-        jTextField4.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
+        txtActividad4.setBackground(new java.awt.Color(220, 201, 201));
+        txtActividad4.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
 
         jLabel13.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
         jLabel13.setText("Actividad N°5");
 
-        jTextField5.setBackground(new java.awt.Color(220, 201, 201));
-        jTextField5.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
+        txtActividad5.setBackground(new java.awt.Color(220, 201, 201));
+        txtActividad5.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
 
         jLabel14.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
         jLabel14.setText("Actividad N°6");
 
-        jTextField6.setBackground(new java.awt.Color(220, 201, 201));
-        jTextField6.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
+        txtActividad6.setBackground(new java.awt.Color(220, 201, 201));
+        txtActividad6.setFont(new java.awt.Font("Myanmar Text", 0, 14)); // NOI18N
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -112,27 +112,27 @@ public class Actividades extends javax.swing.JFrame {
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel14)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtActividad6, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel13)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtActividad5, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel12)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtActividad4, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel11)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtActividad3, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel9)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(txtActividad, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel10)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(txtActividad2, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(28, 28, 28))
         );
         jPanel2Layout.setVerticalGroup(
@@ -143,27 +143,27 @@ public class Actividades extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel9)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtActividad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(26, 26, 26)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel10)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtActividad2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(28, 28, 28)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel11)
-                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtActividad3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(27, 27, 27)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtActividad4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel12))
                 .addGap(29, 29, 29)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel13)
-                    .addComponent(jTextField5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtActividad5, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(26, 26, 26)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel14)
-                    .addComponent(jTextField6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtActividad6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(47, Short.MAX_VALUE))
         );
 
@@ -171,24 +171,24 @@ public class Actividades extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Actividades Diarias");
 
-        jButton1.setBackground(new java.awt.Color(255, 204, 0));
-        jButton1.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
-        jButton1.setText("Listar");
-        jButton1.setActionCommand("");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Listar.setBackground(new java.awt.Color(255, 204, 0));
+        Listar.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
+        Listar.setText("Listar");
+        Listar.setActionCommand("");
+        Listar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                ListarActionPerformed(evt);
             }
         });
 
-        jButton2.setBackground(new java.awt.Color(204, 153, 0));
-        jButton2.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
-        jButton2.setText("Nueva Lista");
-        jButton2.setAlignmentY(0.0F);
-        jButton2.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        NuevaLista.setBackground(new java.awt.Color(204, 153, 0));
+        NuevaLista.setFont(new java.awt.Font("Myanmar Text", 1, 14)); // NOI18N
+        NuevaLista.setText("Nueva Lista");
+        NuevaLista.setAlignmentY(0.0F);
+        NuevaLista.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        NuevaLista.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                NuevaListaActionPerformed(evt);
             }
         });
 
@@ -203,9 +203,9 @@ public class Actividades extends javax.swing.JFrame {
                         .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(205, 205, 205)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(Listar, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(88, 88, 88)
-                        .addComponent(jButton2))
+                        .addComponent(NuevaLista))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(285, 285, 285)
                         .addComponent(jLabel1)))
@@ -220,8 +220,8 @@ public class Actividades extends javax.swing.JFrame {
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton2))
+                    .addComponent(Listar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(NuevaLista))
                 .addGap(135, 135, 135))
         );
 
@@ -290,21 +290,21 @@ public class Actividades extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_mitRegresarMPActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void ListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ListarActionPerformed
         mostrarActividades();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_ListarActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void NuevaListaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NuevaListaActionPerformed
         ActividadesDiarias actividades = new ActividadesDiarias();
         String[] lista = actividades.generarListaSinRepetir(6);
 
-        jTextField1.setText(lista[0]);
-        jTextField2.setText(lista[1]);
-        jTextField3.setText(lista[2]);
-        jTextField4.setText(lista[3]);
-        jTextField5.setText(lista[4]);
-        jTextField6.setText(lista[5]);
-    }//GEN-LAST:event_jButton2ActionPerformed
+        txtActividad.setText(lista[0]);
+        txtActividad2.setText(lista[1]);
+        txtActividad3.setText(lista[2]);
+        txtActividad4.setText(lista[3]);
+        txtActividad5.setText(lista[4]);
+        txtActividad6.setText(lista[5]);
+    }//GEN-LAST:event_NuevaListaActionPerformed
 
     public static void main(String args[]) {
 
@@ -335,8 +335,8 @@ public class Actividades extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton Listar;
+    private javax.swing.JButton NuevaLista;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
@@ -348,12 +348,6 @@ public class Actividades extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    public javax.swing.JTextField jTextField1;
-    public javax.swing.JTextField jTextField2;
-    public javax.swing.JTextField jTextField3;
-    public javax.swing.JTextField jTextField4;
-    public javax.swing.JTextField jTextField5;
-    public javax.swing.JTextField jTextField6;
     private javax.swing.JMenu mbActividadesD;
     private javax.swing.JMenu mbCliente;
     private javax.swing.JMenu mbPrincipal;
@@ -363,5 +357,11 @@ public class Actividades extends javax.swing.JFrame {
     private javax.swing.JMenuItem mitCrearNR;
     private javax.swing.JMenuItem mitRegistrar;
     private javax.swing.JMenuItem mitRegresarMP;
+    public javax.swing.JTextField txtActividad;
+    public javax.swing.JTextField txtActividad2;
+    public javax.swing.JTextField txtActividad3;
+    public javax.swing.JTextField txtActividad4;
+    public javax.swing.JTextField txtActividad5;
+    public javax.swing.JTextField txtActividad6;
     // End of variables declaration//GEN-END:variables
 }
