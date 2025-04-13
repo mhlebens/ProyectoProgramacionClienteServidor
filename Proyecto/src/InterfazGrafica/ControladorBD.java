404b021e89e62012b3091c204b980a5183f1fefd
Ange3108 <aguatemala40593@ufide.ac.cr>
2025-04-12 22:14:56 -0600

    correcion del constructor

--- Proyecto/src/InterfazGrafica/ControladorBD.java
+++ Proyecto/src/InterfazGrafica/ControladorBD.java
@@ -1,46 +1,35 @@
 package InterfazGrafica;
 
-import BD.BDfrm;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.*;
 import javax.swing.JOptionPane;
-import Entidades.Dueño;
+import Entidades.Cliente;
 import ConectarBD.BotonesFuncionalidad;
 
 public class ControladorBD {
      //instancia de la clase Cliente, porque es la última que recibe la herencia de las clases
     //por lo que recolecta todos los atributos y metodos de las clases anteriores (habitacion, perro y Reserva)
-    private final Dueño reserva;
+    private final Cliente persona;
     //instancia del formularios para manejar la base de datos
-    private final BDfrm formulario;
+    private final RegistroP registro;
+    private final Servicios reserva;
+    private final MenuPrincipal menu;
     //instancia de la clase donde se brinda la funcionalidad de los botones
     private final BotonesFuncionalidad botones;
 
-    public ControladorBD(Dueño reserva, BDfrm formulario, BotonesFuncionalidad botones) {
+    public ControladorBD(Cliente persona, RegistroP registro, Servicios reserva, MenuPrincipal menu, BotonesFuncionalidad botones) {
+        this.persona = persona;
+        this.registro = registro;
         this.reserva = reserva;
-        this.formulario = formulario;
+        this.menu = menu;
         this.botones = botones;
-        //inicializacion de los botones del formulario
-        this.formulario.btnBuscar.addActionListener(this);
-        this.formulario.btnGuardar.addActionListener(this);
-        this.formulario.btnModificar.addActionListener(this);
     }
-    
-     public void iniciar() {
-        formulario.setTitle("Control de Reservas");
-        formulario.setLocationRelativeTo(null);
-        formulario.setVisible(true);
-    }
-     
-      public void limpiar() {
-        formulario.txtCedula.setText("");
-        formulario.txtEmail.setText("");
-        formulario.txtNombre.setText("");
-        formulario.txtTelefono.setText("");
-}
-      //implemntacion de la accion de presionar los botones
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
-    }
+   
+    
+    
+
+   
+
 }
