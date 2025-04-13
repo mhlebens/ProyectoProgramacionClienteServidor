dac191b6b34f113affe5d682efcb0b648469fdcf
mafeh <marifermatah@gmail.com>
2025-04-13 12:53:10 -0600

    Borré el proyecto JAJJAJAJAJAJAJ
    ya subo mi versión

--- Proyecto/src/ConectarBD/BotonesFuncionalidad.java
+++
@@ -1,5 +0,0 @@
-package ConectarBD;
-
-public class BotonesFuncionalidad {
-    
-}
--- Proyecto/src/Servicios/ServicioAdicional.java
+++
@@ -1,6 +0,0 @@
-package Servicios;
-
-public interface ServicioAdicional {
-    void asignarHabitacion();
-    void aplicarDescuento();
-}
